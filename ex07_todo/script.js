570c2d77d25f6df532dcaa8b88a3a525dd7d55f6
Korneel Eeckhout <korneel@pienter.tech>
2018-11-29 21:43:13 +0100

    Init - start code

---
+++ ex07_todo/script.js
@@ -0,0 +1,68 @@
+const todoInput;
+const saveBtn;
+const clearAllBtn;
+const todoList;
+const todoCount;
+const doneList;
+const doneCount;
+
+
+function createNewTodo(text) {
+  // return new .box for #todo-list
+  // (look at html)
+}
+
+function updateTodoCount() {
+  // update #todo-count
+  // with number of todo items in #todo-list
+}
+
+function updateDoneCount() {
+  // update #done-count
+  // with number of done items in #done-list
+}
+
+function updateBothCounts() {
+  // update both counts
+}
+
+function save() {
+  // save value of #todo-input
+  // and make new todo item
+}
+
+function clearAll() {
+  // empty #todo-list
+  // empty #done-list
+}
+
+function doneBtnClick(todoItem) {
+  // move todo-item to #done-list
+  // (look at html)
+}
+
+function boxClick(box) {
+  // if box is .active remove active class
+  // else add active class
+  // only one todo-item can have the class active
+}
+
+function todoListClick(event) {
+  // handle click within #todo-list
+}
+
+function removeBtnClick(doneItem) {
+  // remove doneItem
+}
+
+function doneListClick(event) {
+  // handle click within #done-list
+}
+
+//add eventListeners to
+// saveBtn
+// clearAllBtn
+// todoList
+// doneList
+
+clearAll();
